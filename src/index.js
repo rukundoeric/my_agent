155d43e72f77997e8e7e0a22d1ca4bc7b9a4f2a1
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2020-01-10 20:52:14 +0200

    done setup server

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cron = require("node-cron");
+const db = require("./sequelize/models/index.js");
+const api = require("./api/router/index.js");
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+const { sequelize } = db;
+app.get('/', (req, res) => { res.redirect('/api/welcome'); });
+app.use('/api', api);
+
+app.use((req, res) => {
+    res.status(404).send({
+      status: 404,
+      error: {
+        message: 'Page Not found',
+      }
+    });
+  });
+
+sequelize.sync().then(() => {
+    cron.schedule('*/59 * * * *', () => {
+      purgeWorker();
+    });
+    cron.schedule('*/5 * * * *', () => {
+      sendMailWorker();
+    });
+    app.listen(port, () => {
+      console.log(`Database succesfully connected\nPID: ${process.pid} Server listening on port: ${port} in ${process.env.NODE_ENV} mode`);
+    });
+  });
